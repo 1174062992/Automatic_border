adbf4b2dd7f6a6573defa31b9dc1996112576bf5
xunli <xunli@cmmcc.local>
2018-03-12 10:26:13 +0800

    Initial Commit

---
+++ 拍照自动识别边框/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  拍照自动识别边框
+//
+//  Created by xunli on 2018/3/12.
+//  Copyright © 2018年 caoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 拍照自动识别边框/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  拍照自动识别边框
+//
+//  Created by xunli on 2018/3/12.
+//  Copyright © 2018年 caoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
